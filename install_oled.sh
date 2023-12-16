8d8bc2a3e0e5ffe87d28b2e5d6c538f079a80c74
hockeygoalie35 <hockeygoalie35@samulenas.com>
2023-12-16 00:50:02 -0500

    Refactored rpi_oled.py to multiple classes

--- install_oled.sh
+++ install_oled.sh
@@ -1,6 +1,6 @@
 sudo apt install python3.11-venv
-sudo python -m venv /home/pi/rpi_oled_stats/oled/oled_venv --system-site-packages
-source $HOME/rpi_oled_stats/oled/oled_venv/bin/activate
+sudo python -m venv /home/pi/rpi_oled/oled/oled_venv --system-site-packages
+source $HOME/rpi_oled/oled/oled_venv/bin/activate
 cd oled/oled_venv
 sudo mkdir blinka
 cd blinka
@@ -12,12 +12,11 @@ yes N | sudo -E env PATH=$PATH python3 raspi-blinka.py
 cd ..
 cd ..
 python blinkatest.py
-#/home/pi/oled/oled_venv/bin/python oled_stats.py
+
 sudo cp oled.service /lib/systemd/system/
 
 sudo chmod 644 /lib/systemd/system/oled.service
-sudo chmod +x $HOME/rpi_oled_stats/oled/oled_stats.py
-sudo chmod +x $HOME/rpi_oled_stats/oled/CascadiaCode.ttf
+sudo chmod +x $HOME/rpi_oled/oled/rpi_oled.py
+sudo chmod +x $HOME/rpi_oled/oled/CascadiaCode.ttf
 sudo systemctl daemon-reload
-sudo systemctl enable oled.service
-sudo systemctl start oled.service
+sudo systemctl enable oled.service --now
